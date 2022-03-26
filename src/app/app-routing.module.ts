9a2560dd0f000b2049238f38eeab4d0d1bb7ad59
Hilal Ahmad <hilalahmedbutt@gmail.com>
2022-03-26 09:08:48 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SinglePostComponent } from './single-post/single-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "posts" , component : AllPostsComponent },
+  {path : "posts/:id", component : SinglePostComponent},
+  {path : "**", component : NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { SinglePostComponent } from './single-post/single-post.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllPostsComponent,
+    SinglePostComponent,
+    NotFoundComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
